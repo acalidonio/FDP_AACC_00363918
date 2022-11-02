ee4cac11734aaf668e2011587120fdf2cc0ab3b3
acalidonio <00363918@uca.edu.sv>
2022-11-01 19:35:27 -0600

    Se han agregado comentarios

--- Labo 7/labo7ej1.c
+++ Labo 7/labo7ej1.c
@@ -1,34 +1,45 @@
+// Andre Calidonio - 00363918
+
 #include <stdio.h>
 
 int main()
 {
+    // se abren dos archivos de texto distintos, uno para numeros positivos y otro para negativos
     FILE *a = fopen("positivo.txt", "a");
     FILE *b = fopen("negativo.txt", "a");
 
-    // Es buena practica confirmar que el archivo se ha abierto correctamente
+    // se confirma que se abrio el archivo de los positivos correctamente
     if (a == NULL)
     {
         printf("ERROR!");
         return 1;
     }
 
+    // se confirma que se abrio el archivo de los negativos correctamente
     if (b == NULL)
     {
         printf("ERROR!");
         return 1;
     }
 
+    // se declara el int n
     int n = 1;
+
 
     printf("Ingrese numeros enteros, use 0 para salir: ");
     while (n != 0)
-    {
+    {   
+        // mientras n sea distinto de 0, se pide al usuario ingresar un numero para guardarlo en uno de los documentos
         scanf("%d", &n);
+
+        // si n > 0 entonces se guarda en el archivo de los numeros positivos
         if (n > 0)
         {
             fprintf(a, "%d", n);
             fprintf(a, "\n");
         }
+
+        // si n < 0 entonces se guarda en el archivo de los numeros negativos
         else if (n < 0)
         {
             fprintf(b, "%d", n);
@@ -36,6 +47,7 @@ int main()
         }
     }
 
+    // se cierran los archivos y se sale del programa
     fclose(a);
     fclose(b);
     return 0;
